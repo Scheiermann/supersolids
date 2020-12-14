97508d94c46595fe1ab604ab5f77557c51467c5c
Scheiermann <daniel.scheiermann@stud.uni-hannover.de>
2020-12-15 00:02:49 +0100

    Adds PKGBUILD for arch based systems. Renames package to supersolids

--- setup.py
+++ setup.py
@@ -2,17 +2,19 @@ from setuptools import setup, find_packages
 # create a wheel with python setup.py bdist_wheel
 # from Cython.Build import cythonize
 # To compile: python setup.py build_ext --inplace
+# To upload to TestPyPi python -m twine upload --repository testpypi dist/*
 setup(
-    name='supersolids_notes',
-    version='0.1',
+    name='supersolids',
+    version='0.1.2',
     packages=find_packages(),
-    url='https://github.com/Scheiermann/supersolids_notes',
+    url='https://github.com/Scheiermann/supersolids',
     license='MIT',
     author='Scheiermann',
     author_email='daniel.scheiermann@stud.uni-hannover.de',
-    install_requires=["scipy", "matplotlib", "numpy", "sympy"],
-    scripts=["Animation.py", "constants.py", "functions.py", "main.py", "parallel.py", "Schroedinger.py",
-             "sympy_physics_test.py"],
+    install_requires=["matplotlib", "numpy", "scipy", "sympy"],
+    # scripts=["Animation.py", "constants.py", "functions.py", "main.py", "parallel.py", "Schroedinger.py",
+    #          "sympy_physics_test.py"],
     # ext_modules=cythonize("*.pyx", language_level=3),
+    python_requires='>=3.8',
     description='Notes and script to supersolids'
 )
