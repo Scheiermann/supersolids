b1dc20970f2ef4faa5bf74c17322accc42b6bb5c
Scheiermann <daniel.scheiermann@stud.uni-hannover.de>
2021-02-11 11:54:08 +0100

    Adds pyqt5 to requires, as needed for start of supersolid module

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(this_directory, "README.rst"), encoding="utf-8") as f:
 
 setup(
     name="supersolids",
-    version="0.1.26",
+    version="0.1.27",
     packages=find_packages(),
     package_data={"supersolids": []},
     url="https://github.com/Scheiermann/supersolids",
@@ -26,6 +26,7 @@ setup(
                       "numpy",
                       "mayavi",
                       "psutil",
+                      "pyqt5",
                       "scipy",
                       "sphinx-autoapi",
                       "sphinx-rtd-theme",
