3e11da88406c3798a0b2eb7eebd87a168b1574d4
Scheiermann <daniel.scheiermann@stud.uni-hannover.de>
2021-03-26 14:53:11 +0100

    Pre-Release 0.1.30rc9

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(this_directory, "README.rst"), encoding="utf-8") as f:
 
 setup(
     name="supersolids",
-    version="0.1.30",
+    version="0.1.30rc9",
     packages=find_packages(),
     package_data={"supersolids": []},
     url="https://github.com/Scheiermann/supersolids",
