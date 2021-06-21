dbfd49ce64cc5206b8c858c4b3905c2f0aba295a
Scheiermann <daniel.scheiermann@stud.uni-hannover.de>
2021-06-21 12:04:07 +0200

    Moves deprecated scripts to new dir deprecated

--- supersolids/scripts/__init__.py
+++ supersolids/scripts/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 __all__ = ["density_in_trap",
-           "multi_core",
-           "single_core",
+           "deprecated",
+           "multi_load",
+           "multi_property",
            ]
