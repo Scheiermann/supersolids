5d892b994a3d7970e513cd7b7fe3f02a807e8983
Scheiermann <daniel.scheiermann@stud.uni-hannover.de>
2021-07-13 09:21:50 +0200

    Changes small things for convenience

--- supersolids/scripts/download.sh
+++ supersolids/scripts/download.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env bash
 
+path_anchor_input="itpx:/bigwork/dscheier/supersolids/results/joseph_injunction2/"
+path_anchor_output="/run/media/dsche/ITP Transfer/joseph_injunction2/y_kick/kick_fix_1.0/"
+
 # next_new allows to point to different movies of one batched simulation
 next_new=0
 
 # number of movies in batched simulation
-number=$((30 - next_new))
+number=$((20 - next_new))
 
 # starting number to numerate the output directories
-start=$((1 + next_new))
+start=$((820 + next_new))
 dir_name="movie"
 
 # movie152, 4droplets
 # starting number of enumerated directories to download
-download_start=$((516 + next_new))
+# download_start=$((795 + next_new))
+download_start=$start
 download_dir_name="movie"
 
-max_parallel_download=10
+max_parallel_download=2
 # depending on the server settings, number of connections is limited
 # create array with sequence to batch the number of movies
 download_batch=($(seq $max_parallel_download $max_parallel_download $number))
@@ -31,10 +35,6 @@ then
     # add number to sequence
     download_batch+=($number)
 fi
-
-path_anchor_input="itpx:/bigwork/dscheier/supersolids/results/"
-path_anchor_output="/run/media/dsche/ITP Transfer/j_v_none/half_phase_real/"
-
 
 echo ${download_batch[*]}
 
