f2cf2174b951351dc189967eccdd319217f9bc1f
Scheiermann <daniel.scheiermann@stud.uni-hannover.de>
2021-07-28 12:33:22 +0200

    Merges multi_simulate_real by using flag load_from_multi

--- supersolids/scripts/multi_simulate.py
+++ supersolids/scripts/multi_simulate.py
@@ -32,7 +32,7 @@ movie_string = "movie"
 counting_format = "%03d"
 movie_number = 151
 files2last = 324
-movie_now = f"{movie_string}{counting_format % movie_number}"
+load_from_multi = True
 
 func_filename = "distort.txt"
 
@@ -46,16 +46,28 @@ for v in np.arange(v_start, v_end, v_step):
         func_list.append([])
         v_string = round(v, ndigits=5)
         d_string = round(d, ndigits=5)
+        # d_string = 0.0001 * 10.0 ** round(d, ndigits=5)
 
+        # V = f"lambda x, y, z: {v_string} * np.sin(np.pi*x/{d_string}) ** 2"
+        # V = f"lambda x, y, z: {v_string} * np.sin( (np.pi/4.0) + (np.pi*x/{d_string}) )"
+        # V = f"lambda x, y, z: {v_string} * np.sin( (np.pi*x/{d_string}) )"
+        V = f"lambda x, y, z: {v_string} * np.exp(-((x ** 2.0) /{d_string} ** 2.0) )"
+        func_list[j_counter].append(V)
+
+        noise_func = f"lambda gauss, k: 1.0"
+        # noise_func=f"lambda gauss, k: np.concatenate(\
+        #     (np.exp(-1.0j * np.mgrid[-10: 10: complex(0, 256), -5: 5: complex(0, 128), -4: 4: complex(0, 32)][1][:128, :128, :] * {d_string} * (1.0 + 2.0 * k * np.pi /4.0)),\
+        #      np.exp(1.0j * np.mgrid[-10: 10: complex(0, 256), -5: 5: complex(0, 128), -4: 4: complex(0, 32)][1][128:, :, :] * {d_string} * (1.0 + 2.0 * k * np.pi /4.0))),\
+        #     axis=0) * gauss"
+        func_list[j_counter].append(f"-noise_func='{noise_func}' ")
+
+        if load_from_multi:
+            movie_number_now = movie_number + j_counter
+        else:
+            movie_number_now = movie_number
+
+        movie_now = f"{movie_string}{counting_format % movie_number_now}"
         movie_number_after = movie_number + files2last + j_counter
-
-        # func = f"lambda x, y, z: {v_string} * np.sin(np.pi*x/{d_string}) ** 2"
-        # func = f"lambda x, y, z: {v_string} * np.sin( (np.pi/4.0) + (np.pi*x/{d_string}) )"
-        # func = f"lambda x, y, z: {v_string} * np.sin( (np.pi*x/{d_string}) )"
-        func = f"lambda x, y, z: {v_string} * np.exp(-((x ** 2.0) /{d_string} ** 2.0) )"
-
-        func_list[j_counter].append(func)
-
         movie_after = f"{movie_string}{counting_format % movie_number_after}"
 
         dir_path_func = Path(dir_path, movie_after)
@@ -104,9 +116,16 @@ echo $(which pip3)
 /bigwork/dscheier/miniconda3/bin/python3.8 -m supersolids.tools.simulate_npz \
 -Res='{{"x": 256, "y": 128, "z": 32}}' \
 -Box='{{"x0": -10, "x1": 10, "y0": -5, "y1": 5, "z0": -4, "z1": 4}}' \
--max_timesteps={max_timesteps} -dt={dt} -steps_per_npz={steps_per_npz} -accuracy={accuracy} \
--dir_name_load={movie_now} -dir_name_result={movie_after} -filename_npz={file_name} \
--dir_path={dir_path} -V='{func}' \
+-max_timesteps={max_timesteps} \
+-dt={dt} \
+-steps_per_npz={steps_per_npz} \
+-accuracy={accuracy} \
+-dir_name_load={movie_now} \
+-dir_name_result={movie_after} \
+-filename_npz={file_name} \
+-dir_path={dir_path} \
+-noise_func='{noise_func}'\
+-V='{V}' \
 --offscreen \
 --V_reload \
 -noise_func=lambda gauss, k: np.concatenate(
@@ -114,9 +133,8 @@ echo $(which pip3)
 np.exp(1.0j * np.mgrid[-10: 10: complex(0, 256), -5: 5: complex(0, 128), -4: 4: complex(0, 32)][1][128:, :, :] * (1.0 + 2.0 * k * np.pi /4.0))),
 axis=0) * gauss
 
-# -noise_func=lambda gauss, k: np.concatenate((np.ones(shape=(128,128,32)) * np.exp(-1.0j * (1.0 + 2.0 * k * np.pi /4.0)), np.ones(shape=(128,128,32))), axis=0) * gauss
-# -noise -0.1 0.1 -noise_func='lambda x: np.exp(1.0j * x)' --real_time
-# -noise_func=lambda gauss, k: np.exp(-1.0j * (4.0 + k * 2.0 * np.pi /4.0) * gauss) -neighborhood 0.02 4
+# --real_time
+# -neighborhood 0.02 4
 """
         print(heredoc)
 
