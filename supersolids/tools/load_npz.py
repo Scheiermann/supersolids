f77042ada2afe16e50dc13c2811fa1dbc8bc8beb
Scheiermann <daniel.scheiermann@stud.uni-hannover.de>
2021-07-28 11:16:15 +0200

    Refactors args to flag_args

--- supersolids/tools/load_npz.py
+++ supersolids/tools/load_npz.py
@@ -22,18 +22,18 @@ from supersolids.Animation.Animation import Animation
 from supersolids.Animation import MayaviAnimation
 
 
-def load_npz(args):
-    slice_x = args.slice_indices["x"]
-    slice_y = args.slice_indices["y"]
-    slice_z = args.slice_indices["z"]
+def load_npz(flag_args):
+    slice_x = flag_args.slice_indices["x"]
+    slice_y = flag_args.slice_indices["y"]
+    slice_z = flag_args.slice_indices["z"]
 
     try:
-        dir_path = Path(args.dir_path).expanduser()
+        dir_path = Path(flag_args.dir_path).expanduser()
     except Exception:
-        dir_path = args.dir_path
+        dir_path = flag_args.dir_path
 
-    Anim: Animation = Animation(plot_psi_sol=args.plot_psi_sol,
-                                plot_V=args.plot_V,
+    Anim: Animation = Animation(plot_psi_sol=flag_args.plot_psi_sol,
+                                plot_V=flag_args.plot_V,
                                 alpha_psi=0.8,
                                 alpha_psi_sol=0.5,
                                 alpha_V=0.3,
@@ -46,23 +46,23 @@ def load_npz(args):
                                               slice_indices=[slice_x, slice_y, slice_z],
                                               )
 
-    animate_wrapper = mlab.animate(MayAnim.animate_npz, delay=10, ui=args.ui)
+    animate_wrapper = mlab.animate(MayAnim.animate_npz, delay=10, ui=flag_args.ui)
     MayAnimator = animate_wrapper(dir_path=dir_path,
-                                  dir_name=args.dir_name,
-                                  filename_schroedinger=args.filename_schroedinger,
-                                  filename_steps=args.filename_steps,
-                                  steps_format=args.steps_format,
-                                  steps_per_npz=args.steps_per_npz,
-                                  frame_start=args.frame_start,
-                                  arg_slices=args.arg_slices,
-                                  azimuth=args.azimuth,
-                                  elevation=args.elevation,
+                                  dir_name=flag_args.dir_name,
+                                  filename_schroedinger=flag_args.filename_schroedinger,
+                                  filename_steps=flag_args.filename_steps,
+                                  steps_format=flag_args.steps_format,
+                                  steps_per_npz=flag_args.steps_per_npz,
+                                  frame_start=flag_args.frame_start,
+                                  arg_slices=flag_args.arg_slices,
+                                  azimuth=flag_args.azimuth,
+                                  elevation=flag_args.elevation,
                                   )
     mlab.show()
 
     result_path = MayAnim.create_movie(dir_path=MayAnim.dir_path,
                                        input_data_file_pattern="*.png",
-                                       delete_input=args.delete_input)
+                                       delete_input=flag_args.delete_input)
 
 
 def flags(args_array):
@@ -80,7 +80,7 @@ def flags(args_array):
                              "For example the standard naming convention is step_000001.npz, "
                              "the string needed is step_")
     parser.add_argument("-steps_format", type=str, default="%06d",
-                        help="Formating string to enumerate the files. "
+                        help="Formatting string to enumerate the files. "
                              "For example the standard naming convention is step_000001.npz, "
                              "the string needed is percent 06d")
     parser.add_argument("-steps_per_npz", type=int, default=10,
@@ -93,7 +93,7 @@ def flags(args_array):
     parser.add_argument("-azimuth", type=float, default=0.0, help="Phi angle in x-y-plane.")
     parser.add_argument("-elevation", type=float, default=0.0, help="Zenith angle theta in z-axis.")
     parser.add_argument("--plot_psi_sol", default=False, action="store_true",
-                        help="Option to plot the manually given solution for the wavefunction psi")
+                        help="Option to plot the manually given solution for the wave function psi")
     parser.add_argument("--plot_V", default=False, action="store_true",
                         help="Option to plot the external potential of the system (the trap)")
     parser.add_argument("--delete_input", default=False, action="store_true",
@@ -107,10 +107,10 @@ def flags(args_array):
                              "This is handy as adjusting plots is just stable for stopped "
                              "animation.")
 
-    args = parser.parse_args(args_array)
-    print(f"args: {args}")
+    flag_args = parser.parse_args(args_array)
+    print(f"args: {flag_args}")
 
-    return args
+    return flag_args
 
 
 # Script runs, if script is run as main script (called by python *.py)
