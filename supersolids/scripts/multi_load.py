eb2f526c70210ea425f14a7d817713ec87611580
Scheiermann <daniel.scheiermann@stud.uni-hannover.de>
2021-07-28 11:26:25 +0200

    Uses get_step_index to load last (take_last) files in every dir

--- supersolids/scripts/multi_load.py
+++ supersolids/scripts/multi_load.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
 from pathlib import Path
+import numpy as np
 
-from supersolids.tools.load_npz import load_npz, flags
+from supersolids.helper.get_path import get_step_index
+from supersolids.tools.load_npz import load_npz
+from supersolids.tools.load_npz import flags
 
 
 def string_float(s):
@@ -13,8 +16,9 @@ def string_float(s):
 if __name__ == "__main__":
     path_anchor_input = Path("/run/media/dsche/ITP Transfer/joseph_injunction2/y_kick/kick_fix_0.01/")
 
-    frame_start = 1210000
-    # frame_start = 1510000
+    take_last = 30
+    # take_last = np.inf
+
     steps_per_npz = 1000
 
     movie_string = "movie"
@@ -22,7 +26,9 @@ if __name__ == "__main__":
     movie_start = 1
     movie_end = 20
 
+    filename_steps = "step_"
     steps_format = "%07d"
+    filename_pattern = ".npz"
 
     azimuth = 0.0
     elevation = 0.0
@@ -32,15 +38,31 @@ if __name__ == "__main__":
     ui = False
 
     for i in range(movie_start, movie_end + 1):
+        path_in = Path(path_anchor_input, movie_string + f"{counting_format % i}")
+        files = sorted([x for x in path_in.glob("*" + filename_pattern) if x.is_file()])
+        if len(files) > take_last:
+            files_last = files[-take_last]
+        else:
+            try:
+                files_last = files[0]
+            except IndexError:
+                # no files in dir
+                print(f'{str(Path(path_in)) + "/*" + filename_pattern} not found. Skipping.')
+                continue
+
+        frame_start = get_step_index(files_last,
+                                     filename_prefix=filename_steps,
+                                     file_pattern=filename_pattern)
+
         command = ["python", "-m", "supersolids.tools.load_npz"]
         flags_given = [f"-dir_path={path_anchor_input}",
-                 f"-dir_name={movie_string}{counting_format % i}",
-                 f"-frame_start={frame_start}",
-                 f"-steps_per_npz={steps_per_npz}",
-                 f"-steps_format={steps_format}",
-                 f'-slice_indices={{"x":127,"y":63,"z":15}}',
-                 f"-azimuth={azimuth}",
-                 f"-elevation={elevation}"]
+                       f"-dir_name={movie_string}{counting_format % i}",
+                       f"-frame_start={frame_start}",
+                       f"-steps_per_npz={steps_per_npz}",
+                       f"-steps_format={steps_format}",
+                       f'-slice_indices={{"x":127,"y":63,"z":15}}',
+                       f"-azimuth={azimuth}",
+                       f"-elevation={elevation}"]
 
         if arg_slices:
             flags_given.append("--arg_slices")
