14bf6ffac1efe6b0fd0bac725c95fb14781910dd
Scheiermann <daniel.scheiermann@stud.uni-hannover.de>
2021-09-08 23:35:28 +0200

    Adds missing scripts

--- supersolids/scripts/__init__.py
+++ supersolids/scripts/__init__.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
-__all__ = ["density_in_trap",
+__all__ = ["cp_plots",
+           "density_in_trap",
            "deprecated",
+           "download_last",
+           "multi_begin",
            "multi_load",
            "multi_property",
+           "multi_simulate",
            ]
