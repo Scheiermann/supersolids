91c32b2b8769e0754d6cd4a8469f43a819b271c8
Scheiermann <daniel.scheiermann@itp.uni-hannover.de>
2021-11-06 11:55:41 +0100

    Fixes load_summary. Adds plot_contour. Adjusts functions to new flags.

--- supersolids/tools/get_System_at_npz.py
+++ supersolids/tools/get_System_at_npz.py
@@ -11,6 +11,7 @@ from matplotlib import pyplot as plt
 
 from supersolids.Schroedinger import Schroedinger
 from supersolids.SchroedingerMixture import SchroedingerMixture
+from supersolids.helper.get_path import get_path
 from supersolids.tools.track_property import property_check, get_property
 
 
@@ -31,7 +32,8 @@ def get_System_at_npz(dir_path: Path = Path("~/supersolids/results").expanduser(
         # WARNING: this is just the input Schroedinger at t=0
         System_loaded: Schroedinger = dill.load(file=f)
 
-    SystemSummary, summary_name = System_loaded.use_summary(summary_name=args.summary_name)
+    System_loaded = System_loaded.load_summary(input_path, steps_format, frame)
+
     try:
         # get the psi_val of Schroedinger at other timesteps (t!=0)
         psi_val_path = Path(input_path, filename_steps + steps_format % frame + ".npz")
@@ -54,7 +56,7 @@ def get_System_at_npz(dir_path: Path = Path("~/supersolids/results").expanduser(
     return System_loaded
 
 
-def plot_System_at_npz(args):
+def get_property_all(args):
     try:
         dir_path = Path(args.dir_path).expanduser()
     except Exception:
@@ -62,12 +64,23 @@ def plot_System_at_npz(args):
 
     property_values = []
     for i in range(args.dir_start, args.dir_end + 1):
+        if args.frame is None:
+            _, last_index, _, _ = get_path(Path(dir_path,
+                                                args.dir_name + f"{args.dir_name_format % i}"
+                                                ),
+                                           search_prefix=args.filename_steps,
+                                           counting_format=args.steps_format,
+                                           file_pattern=".npz")
+            frame = last_index
+        else:
+            frame = args.frame
+
         System = get_System_at_npz(dir_path=dir_path,
-                                   dir_name=f"{args.dir_name % i}",
+                                   dir_name=f"{args.dir_name + args.dir_name_format % i}",
                                    filename_schroedinger=args.filename_schroedinger,
                                    filename_steps=args.filename_steps,
                                    steps_format=args.steps_format,
-                                   frame=args.frame,
+                                   frame=frame,
                                    )
         property_values.append(property_check(get_property(System, args.property_name),
                                               args.property_name,
@@ -88,36 +101,64 @@ def plot_System_at_npz(args):
     print(f"Extracted v_0: {v_0_list}")
     print(f"Extracted delta: {delta_list}")
     print(f"Extracted property_values: {property_values}")
-    print(f"Extracted property_values[0].shape: {property_values[0].shape}")
     print(f"Extracted len(property_values): {len(property_values)}")
 
+    return dir_path, v_0_list, delta_list, property_values
+
+
+def plot_System_at_npz(args, dir_path, v_0_list, delta_list, property_values):
     try:
         dim = property_values[0].shape[0]
+        print(f"Extracted property_values[0].shape: {property_values[0].shape}")
+        print(f"Extracted property_values[0].shape[0]: {dim}")
     except Exception:
         dim = 1
 
+    fig, ax = plt.subplots(figsize=(16, 9))
     if dim == 1:
-        plt.plot(property_values, "x-")
+        ax.plot(property_values, "x-")
     else:
         labels = []
         for i in range(0, dim):
             labels.append(str(i + 1))
-            plt.plot(property_values[0].T[i], "x-", label=labels[i])
-        plt.legend()
+            ax.plot(property_values[0].T[i], "x-", label=labels[i])
+        ax.legend()
 
     v_d = zip(v_0_list, delta_list)
-    plt.xlabel(f"Amplitude of distortion (v_0)")
-    plt.ylabel(f"{args.property_name}")
-    plt.xticks(np.arange(len(v_0_list)), [round(elem, 3) for elem in v_0_list], rotation=90)
-    secx = plt.Axes.secondary_xaxis(plt.gca(), "top")
+    # ax.xlabel(f"Amplitude of distortion (v_0)")
+    ax.set_xlabel(r"Ratio $\frac{N_2}{N}$ (v_0)")
+    ax.set_ylabel(f"{args.property_name}")
+    ax.set_xticks(np.arange(len(v_0_list)))
+    ax.set_xticklabels([round(elem, 3) for elem in v_0_list])
+    ax.tick_params(axis="x", rotation=90)
+    secx = ax.secondary_xaxis("top")
     secx.set_xticks(np.arange(len(delta_list)))
     secx.set_xticklabels([round(elem, 3) for elem in delta_list])
     secx.tick_params(axis="x", rotation=90)
-    secx.set_xlabel(f"Frequency of distortion (delta)")
-    plt.grid()
-    plt.title(f"with property_args: {args.property_args}")
+    # secx.set_xlabel(f"Frequency of distortion (delta)")
+    secx.set_xlabel(r"Scatter length $a_{12}$ (delta)")
+    ax.grid()
+    ax.set_title(f"with property_args: {args.property_args}")
     if args.property_name:
-        plt.savefig(Path(dir_path, f"{args.property_name}"))
+        fig.savefig(Path(dir_path, f"{args.property_name}"))
+
+
+def plot_contour(args, dir_path, property_values):
+    v_start, v_end, v_step = args.v_arange
+    d_start, d_end, d_step = args.d_arange
+    x = np.arange(v_start, v_end, v_step)
+    y = np.arange(d_start, d_end, d_step)
+    X, Y = np.meshgrid(x, y, indexing="ij")
+
+    property_arr = np.array(property_values)
+    for i in range(0, len(property_values[0])):
+        fig, ax = plt.subplots()
+        Z = np.reshape(property_arr[:, i], (len(x), len(y)))
+        im = ax.pcolormesh(Y, X, Z, shading="auto")
+        fig.colorbar(im)
+        ax.set_title(f"with property_args: {args.property_args}")
+        if args.property_name:
+            fig.savefig(Path(dir_path, f"{args.property_name}" + "_contour_" + f"{i}"))
 
 
 def flags(args_array):
@@ -125,9 +166,12 @@ def flags(args_array):
                                                  "and create movie.")
     parser.add_argument("-dir_path", metavar="dir_path", type=str, default="~/supersolids/results",
                         help="Absolute path to load npz data from")
-    parser.add_argument("-dir_name", metavar="dir_name", type=str, default="movie" + "%03d",
+    parser.add_argument("-dir_name", metavar="dir_name", type=str, default="movie",
                         help="Formatting of directory name where the files to load lie. "
                              "Use movie%03d for dir_names like movie001.")
+    parser.add_argument("-dir_name_format", metavar="dir_name_format", type=str, default="%03d",
+                        help="Formatting of number of directory name. "
+                             "Use %03d for dir_names like movie001.")
     parser.add_argument("-filename_schroedinger", metavar="filename_schroedinger", type=str,
                         default="schroedinger.pkl",
                         help="Name of file, where the Schroedinger object is saved")
@@ -168,4 +212,6 @@ def flags(args_array):
 # Script runs, if script is run as main script (called by python *.py)
 if __name__ == "__main__":
     args = flags(sys.argv[1:])
-    plot_System_at_npz(args)
+    dir_path, v_0_list, delta_list, property_values = get_property_all(args)
+    plot_System_at_npz(args, dir_path, v_0_list, delta_list, property_values)
+    plot_contour(args, dir_path, property_values)
