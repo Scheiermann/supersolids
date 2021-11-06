cd1f30d5cfe17c0c7733f788827f43efa5e20581
Scheiermann <daniel.scheiermann@itp.uni-hannover.de>
2021-11-06 11:58:39 +0100

    Adds periodic_system.

--- supersolids/scripts/multi_last_frame.py
+++ supersolids/scripts/multi_last_frame.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python
 import shutil
 from pathlib import Path
+from typing import List
+
+from PIL import Image
+
 from supersolids.helper.get_path import get_path
+
+
+def periodic_system(path_in_list: List[Path], path_out: Path, nrow: int, ncol: int):
+    fs = []
+    for i, path_in in enumerate(path_in_list):
+        im = Image.open(path_in, 'r')
+        fs.append(im)
+
+    x, y = fs[0].size
+    cvs = Image.new('RGB', (x * ncol, y * nrow))
+    for i in range(len(fs)):
+        px, py = x * (i % ncol), y * int(i / ncol)
+        cvs.paste(fs[i], (px, py))
+
+    cvs.save(path_out, format='png')
 
 
 # Script runs, if script is run as main script (called by python *.py)
@@ -19,14 +38,20 @@ if __name__ == "__main__":
     movie_start = 1
     movie_end = 70
 
+    ncomponents = 2
+    nrow = 10
+    ncol = 7
+
+
     dir_name = "movie"
     counting_format = "%03d"
 
     dir_name_png = "movie"
     counting_format_png = "%03d"
 
-    dir_suffix = "last_frame"
-    filename_out = "last_frame"
+    movie_take_last: int = 2
+    dir_suffix = "last_frame_1"
+    filename_out = "last_frame_1"
     filename_extension = ".png"
 
     file_suffix = ""
@@ -38,6 +63,7 @@ if __name__ == "__main__":
     if not path_anchor_output.is_dir():
         path_anchor_output.mkdir(parents=True)
 
+    path_out_list: List[Path] = []
     for movie_number in range(movie_start, movie_end + 1):
         # gets last movie with animations of each movie
         path_last_movie_png, _, _, _ = get_path(
@@ -55,6 +81,9 @@ if __name__ == "__main__":
             file_pattern=filename_extension,
             )
         print(f"{path_last_png}")
-        shutil.copy(path_last_png,
-                    Path(path_anchor_output, f"{filename_out}_{movie_number}{filename_extension}")
-                    )
+        path_out: Path = Path(path_anchor_output, f"{filename_out}_{movie_number}{filename_extension}")
+        path_out_list.append(path_out)
+        shutil.copy(path_last_png, path_out)
+
+    path_out_periodic: Path = Path(path_anchor_output, f"periodic_system.png")
+    periodic_system(path_in_list=path_out_list, path_out=path_out_periodic, nrow=nrow, ncol=ncol)
