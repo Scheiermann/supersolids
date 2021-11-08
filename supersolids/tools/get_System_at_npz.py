bc26d524c7473bc9d0b5b64ae4878d9ece1d24e1
Scheiermann <daniel.scheiermann@itp.uni-hannover.de>
2021-11-08 13:21:04 +0100

    Restructures tool

--- supersolids/tools/get_System_at_npz.py
+++ supersolids/tools/get_System_at_npz.py
@@ -56,6 +56,31 @@ def get_System_at_npz(dir_path: Path = Path("~/supersolids/results").expanduser(
     return System_loaded
 
 
+def get_property_one(args, dir_path: Path, i: int):
+    if args.frame is None:
+        _, last_index, _, _ = get_path(Path(dir_path, args.dir_name_list[i]),
+                                       search_prefix=args.filename_steps,
+                                       counting_format=args.steps_format,
+                                       file_pattern=".npz")
+        frame = last_index
+    else:
+        frame = args.frame
+
+    System = get_System_at_npz(dir_path=dir_path,
+                               dir_name=f"{args.dir_name_list[i]}",
+                               filename_schroedinger=args.filename_schroedinger,
+                               filename_steps=args.filename_steps,
+                               steps_format=args.steps_format,
+                               frame=frame,
+                               )
+    property_one = property_check(get_property(System, args.property_name),
+                                  args.property_name,
+                                  args.property_func,
+                                  args.property_args_list[i])
+
+    return property_one
+
+
 def get_property_all(args):
     try:
         dir_path = Path(args.dir_path).expanduser()
@@ -63,50 +88,17 @@ def get_property_all(args):
         dir_path = args.dir_path
 
     property_values = []
-    for i in range(args.dir_start, args.dir_end + 1):
-        if args.frame is None:
-            _, last_index, _, _ = get_path(Path(dir_path,
-                                                args.dir_name + f"{args.dir_name_format % i}"
-                                                ),
-                                           search_prefix=args.filename_steps,
-                                           counting_format=args.steps_format,
-                                           file_pattern=".npz")
-            frame = last_index
-        else:
-            frame = args.frame
+    for i in range(0, len(args.dir_name_list)):
+        property_one = get_property_one(args, dir_path, i)
+        property_values.append(property_one)
 
-        System = get_System_at_npz(dir_path=dir_path,
-                                   dir_name=f"{args.dir_name + args.dir_name_format % i}",
-                                   filename_schroedinger=args.filename_schroedinger,
-                                   filename_steps=args.filename_steps,
-                                   steps_format=args.steps_format,
-                                   frame=frame,
-                                   )
-        property_values.append(property_check(get_property(System, args.property_name),
-                                              args.property_name,
-                                              args.property_func,
-                                              args.property_args)
-                               )
-
-    v_start, v_end, v_step = args.v_arange
-    d_start, d_end, d_step = args.d_arange
-
-    v_0_list = []
-    delta_list = []
-    for i, v_0 in enumerate(np.arange(v_start, v_end, v_step)):
-        for j, delta in enumerate(np.arange(d_start, d_end, d_step)):
-            v_0_list.append(v_0)
-            delta_list.append(delta)
-
-    print(f"Extracted v_0: {v_0_list}")
-    print(f"Extracted delta: {delta_list}")
     print(f"Extracted property_values: {property_values}")
     print(f"Extracted len(property_values): {len(property_values)}")
 
-    return dir_path, v_0_list, delta_list, property_values
+    return dir_path, property_values
 
 
-def plot_System_at_npz(args, dir_path, v_0_list, delta_list, property_values):
+def plot_System_at_npz(args, dir_path, var1, var2, property_values):
     try:
         dim = property_values[0].shape[0]
         print(f"Extracted property_values[0].shape: {property_values[0].shape}")
@@ -124,37 +116,28 @@ def plot_System_at_npz(args, dir_path, v_0_list, delta_list, property_values):
             ax.plot(property_values[0].T[i], "x-", label=labels[i])
         ax.legend()
 
-    v_d = zip(v_0_list, delta_list)
-    # ax.xlabel(f"Amplitude of distortion (v_0)")
-    ax.set_xlabel(r"Ratio $\frac{N_2}{N}$ (v_0)")
+    ax.set_xlabel(r"Ratio $\frac{N_2}{N}$ (var1)")
     ax.set_ylabel(f"{args.property_name}")
-    ax.set_xticks(np.arange(len(v_0_list)))
-    ax.set_xticklabels([round(elem, 3) for elem in v_0_list])
+    ax.set_xticks(np.arange(len(var1)))
+    ax.set_xticklabels([round(elem, 3) for elem in var1])
     ax.tick_params(axis="x", rotation=90)
     secx = ax.secondary_xaxis("top")
-    secx.set_xticks(np.arange(len(delta_list)))
-    secx.set_xticklabels([round(elem, 3) for elem in delta_list])
+    secx.set_xticks(np.arange(len(var2)))
+    secx.set_xticklabels([round(elem, 3) for elem in var2])
     secx.tick_params(axis="x", rotation=90)
-    # secx.set_xlabel(f"Frequency of distortion (delta)")
-    secx.set_xlabel(r"Scatter length $a_{12}$ (delta)")
+    secx.set_xlabel(r"Scatter length $a_{12}$ (var2)")
     ax.grid()
-    ax.set_title(f"with property_args: {args.property_args}")
+    # ax.set_title(f"with property_args: {args.property_args}")
     if args.property_name:
         fig.savefig(Path(dir_path, f"{args.property_name}"))
 
 
-def plot_contour(args, dir_path, property_values):
-    v_start, v_end, v_step = args.v_arange
-    d_start, d_end, d_step = args.d_arange
-    x = np.arange(v_start, v_end, v_step)
-    y = np.arange(d_start, d_end, d_step)
-    X, Y = np.meshgrid(x, y, indexing="ij")
-
+def plot_contour(args, dir_path, var1_mesh, var2_mesh, property_values):
     property_arr = np.array(property_values)
     for i in range(0, len(property_values[0])):
         fig, ax = plt.subplots()
-        Z = np.reshape(property_arr[:, i], (len(x), len(y)))
-        im = ax.pcolormesh(Y, X, Z, shading="auto")
+        Z = np.reshape(property_arr[:, i], var1_mesh.shape)
+        im = ax.pcolormesh(var2_mesh, var1_mesh, Z, shading="auto")
         fig.colorbar(im)
         ax.set_title(f"with property_args: {args.property_args}")
         if args.property_name:
@@ -166,12 +149,10 @@ def flags(args_array):
                                                  "and create movie.")
     parser.add_argument("-dir_path", metavar="dir_path", type=str, default="~/supersolids/results",
                         help="Absolute path to load npz data from")
-    parser.add_argument("-dir_name", metavar="dir_name", type=str, default="movie",
+    parser.add_argument("-dir_name_list", metavar="dir_name", type=str, default="movie001",
+                        action="store", nargs="*",
                         help="Formatting of directory name where the files to load lie. "
                              "Use movie%03d for dir_names like movie001.")
-    parser.add_argument("-dir_name_format", metavar="dir_name_format", type=str, default="%03d",
-                        help="Formatting of number of directory name. "
-                             "Use %03d for dir_names like movie001.")
     parser.add_argument("-filename_schroedinger", metavar="filename_schroedinger", type=str,
                         default="schroedinger.pkl",
                         help="Name of file, where the Schroedinger object is saved")
@@ -184,14 +165,10 @@ def flags(args_array):
                              "For example the standard naming convention is step_0000001.npz, "
                              "the string needed is %07d")
     parser.add_argument("-frame", type=json.loads, default=None, help="Counter of first saved npz.")
-    parser.add_argument("-dir_start", type=int, default=1, help="Counter of first dir name.")
-    parser.add_argument("-dir_end", type=int, default=2, help="Counter of last dir name.")
-    parser.add_argument("-v_arange", metavar=("v_start", "v_end", "v_step"),
-                        type=json.loads, default=None, action='store',
-                        nargs=3, help="v_start, v_end, v_step, to match directories properly.")
-    parser.add_argument("-d_arange", metavar=("d_start", "d_end", "d_step"),
-                        type=json.loads, default=None, action='store',
-                        nargs=3, help="d_start, d_end, d_step, to match directories properly.")
+    parser.add_argument("-var1_arange", type=json.loads, default=None, action='store',
+                        nargs="*", help="List of values for var1 in phasediagram.")
+    parser.add_argument("-var2_arange", type=json.loads, default=None, action='store',
+                        nargs="*", help="List of values for var1 in phasediagram.")
     parser.add_argument("-property_name", type=str, default="mu",
                         help="Name of property to get from the Schroedinger object.")
     parser.add_argument("--property_func", default=False, action="store_true",
@@ -199,8 +176,8 @@ def flags(args_array):
                              "an Schroedinger object."
                              "If used, flag property_name will be a interpreted as method of an "
                              "Schroedinger object.")
-    parser.add_argument("--property_args", default=[], type=json.loads,
-                        action='store', nargs="*",
+    parser.add_argument("--property_args_list", default=[], type=json.loads,
+                        action='append', nargs="*",
                         help="Arguments for property_name, if property_func is used.")
 
     args = parser.parse_args(args_array)
@@ -212,6 +189,8 @@ def flags(args_array):
 # Script runs, if script is run as main script (called by python *.py)
 if __name__ == "__main__":
     args = flags(sys.argv[1:])
-    dir_path, v_0_list, delta_list, property_values = get_property_all(args)
-    plot_System_at_npz(args, dir_path, v_0_list, delta_list, property_values)
-    plot_contour(args, dir_path, property_values)
+    var1_mesh, var2_mesh = np.meshgrid(args.var1_arange, args.var2_arange, indexing="ij")
+
+    dir_path, property_values = get_property_all(args)
+    plot_System_at_npz(args, dir_path, args.var1_arange, args.var2_arange, property_values)
+    plot_contour(args, dir_path, var1_mesh, var2_mesh, property_values)
