0cd5a1d387a2f2018da4aac00a6c9df761a753bc
Scheiermann <daniel.scheiermann@itp.uni-hannover.de>
2021-12-01 15:03:10 +0100

    Removes usage of numba_compiled, instead uses numbas.py

--- supersolids/helper/numbas.py
+++ supersolids/helper/numbas.py
@@ -12,8 +12,62 @@ Function speeded up with numba
 import numba
 import numpy as np
 
-from numba import njit, types, f8, c16, objmode
+from numba import njit, types, f8, c16
 from typing import Callable
+
+
+@njit('c16[:,:,:](f8[:,:,:], f8, f8, c16[:,:,:], f8[:,:,:], f8[:,:,:])')
+def get_H_pot_exponent_terms_jit(V_val: np.ndarray,
+                                 a_dd_factor: float,
+                                 a_s_factor: float,
+                                 dipol_term: np.ndarray,
+                                 contact_interaction: np.ndarray,
+                                 mu_lhy: np.ndarray) -> np.ndarray:
+    return (V_val
+            + a_dd_factor * dipol_term
+            + a_s_factor * contact_interaction
+            + mu_lhy
+            )
+
+
+@njit('c16[:,:,:](c16, f8, c16[:,:,:], f8)')
+def get_H_pot_jit(U: np.complex, dt: float, terms: np.ndarray,
+                  split_step: float = 0.5) -> np.ndarray:
+    return np.exp(U * (split_step * dt) * terms)
+
+
+@njit('f8[:](f8[:], f8, f8, f8, f8)')
+def f_lam(A: np.ndarray, lam: float,
+          eta_aa: float, eta_bb: float, eta_ab: float) -> np.ndarray:
+    return (eta_aa * A
+            + eta_bb * (1.0 - A)
+            + lam * np.sqrt((eta_aa * A - eta_bb * (1.0 - A)) ** 2.0
+                            + 4.0 * eta_ab ** 2.0 * A * (1.0 - A)))
+
+
+@njit('f8[:](f8[:], f8, f8, f8, f8)')
+def eta_dVdna_jit(A: np.ndarray, lam: float,
+                  eta_aa: float, eta_bb: float, eta_ab: float) -> np.ndarray:
+    return (eta_aa
+            + lam * (eta_aa * (eta_aa * A - eta_bb * (1 - A))
+                     + 2 * eta_ab ** 2 * (1 - A)
+                     )
+            / np.sqrt((eta_aa * A - eta_bb * (1 - A)) ** 2
+                      + 4 * eta_ab ** 2 * A * (1 - A)
+                      )
+            )
+
+
+@njit('f8[:](f8[:], f8, f8, f8, f8)')
+def eta_dVdnb_jit(A: np.ndarray, lam: float,
+                  eta_aa: float, eta_bb: float, eta_ab: float) -> np.ndarray:
+    return (eta_bb
+            + lam * (eta_bb * (eta_bb * (1 - A) - eta_aa * A)
+                     + 2 * eta_ab ** 2 * A)
+            / np.sqrt((eta_aa * A - eta_bb * (1 - A)) ** 2
+                      + 4 * eta_ab ** 2 * A * (1 - A)
+                      )
+            )
 
 
 @numba.vectorize('f8(c16)')
