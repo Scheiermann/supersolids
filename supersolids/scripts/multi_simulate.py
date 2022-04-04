422643593b15bf0fe698fb392936d05da8ce07e2
Scheiermann <daniel.scheiermann@itp.uni-hannover.de>
2022-04-04 20:58:52 +0200

    Pre-Release 0.1.34rc31

--- supersolids/scripts/multi_simulate.py
+++ supersolids/scripts/multi_simulate.py
@@ -11,7 +11,7 @@ import numpy as np
 
 from supersolids.helper.dict2str import dic2str
 
-supersolids_version = "0.1.34rc30"
+supersolids_version = "0.1.34rc31"
 dir_path = Path("/bigwork/dscheier/results/begin_gpu/")
 
 slurm: bool = True
